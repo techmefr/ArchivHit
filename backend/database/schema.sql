9b9d4ac212a4bc67c82fac37ab76af83c7403341
techmefr <compigni.gaetan@gmail.com>
2024-01-31 10:08:28 +0100

    create database and tables

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,30 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+CREATE DATABASE IF NOT EXISTS archivHit;
+
+USE archivHit;
+  
+CREATE TABLE `user` (
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(80),
+    `email` VARCHAR(125),
+    `password` VARCHAR(255)
+  );
+
+CREATE TABLE `editor` (
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(80)
 );
+
+CREATE TABLE `game` (
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `type` BOOLEAN,
+    `play_time` INT,
+    `age_min` INT,
+    `age_max` INT,
+    `player_min` INT,
+    `player_max` INT,
+    `user_id` INT,
+    `editor_id` INT,
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (editor_id) REFERENCES editor (id) ON DELETE CASCADE
+  );
